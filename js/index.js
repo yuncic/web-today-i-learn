9c7f93b41cdab838455310072b67b784f6a77940
김상윤 <ksy151020@gmail.com>
2026-03-18 20:20:13 +0900

    feat: 과제 완료

--- js/index.js
+++ js/index.js
@@ -9,4 +9,21 @@ tilForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
   // TODO: 입력값을 가져와서 새 TIL 항목을 만들어 목록에 추가하세요
+
+  const date = document.querySelector("#til-date").value;
+  const title = document.querySelector("#til-title").value;
+  const content = document.querySelector("#til-content").value;
+
+  const newItem = document.createElement("article");
+  newItem.classList.add("til-item");
+
+  newItem.innerHTML = `
+    <time>${date}</time>
+    <h3>${title}</h3>
+    <p>${content}</p>
+  `;
+
+  tilList.prepend(newItem);
+
+  tilForm.reset();
 });
